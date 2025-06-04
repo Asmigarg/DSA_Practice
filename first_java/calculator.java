b14fa484fed04245cb91865fad3f92957acedc41
ASMI GARG <asmigarg569@gmail.com>
2025-06-04 21:15:46 +0530

    Update calculator.java

--- first_java/calculator.java
+++ first_java/calculator.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class que4 {
+public class calculator {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         System.out.println("Enter num1:");
