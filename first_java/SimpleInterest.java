d9435d103f7ea5a321d4b481c077fe6342578d03
ASMI GARG <asmigarg569@gmail.com>
2025-06-04 21:09:49 +0530

    Create SimpleInterest.java

---
+++ first_java/SimpleInterest.java
@@ -0,0 +1,14 @@
+// Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+import java.util.Scanner;
+
+public class que3 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int p=in.nextInt();
+        int t=in.nextInt();
+        int r=in.nextInt();
+        float si=(p*t*r)/100;
+        System.out.println(si);
+    }
+}
