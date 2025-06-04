8c1a012c7a0f720f925ef16b0eb0c5039e71ef34
ASMI GARG <asmigarg569@gmail.com>
2025-06-04 21:09:15 +0530

    Create nameInput.java

---
+++ first_java/nameInput.java
@@ -0,0 +1,12 @@
+// Take name as input and print a greeting message for that particular name.
+
+import java.util.Scanner;
+
+public class que2 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name=in.nextLine();
+        System.out.println("Hello welcome to codeing world "+name);
+    }
+}
